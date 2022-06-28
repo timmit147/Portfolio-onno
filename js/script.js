2073d9b75a3f47bdf281906fc6161e56cb2a0af8
timmit147 <timmeeuwsen@hotmail.nl>
2022-06-28 12:45:53 +0200

    Header working

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Hamburger menu
+
+
+function menu(){
+    const $ = document.querySelector.bind(document)
+
+    $(".hamburger-menu").onclick = function(){
+        $("header").classList.toggle("menu-active");
+    };
+
+    let nav = document.querySelectorAll('header nav a');
+    let i = 1;
+    for (const a of nav) {
+        const item = i;
+        a.onmouseenter = function(){
+            $("header").classList.add(`active-${item}`);
+        };
+        
+        a.onmouseleave = function(){
+            $("header").classList.remove(`active-${item}`);
+        };
+        i++;
+      }
+}
+
+menu()
