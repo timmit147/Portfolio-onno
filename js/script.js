71e2c19cf36ab19a6269aaa1ad3c4010627caf00
timmit147 <timmeeuwsen@hotmail.nl>
2022-06-28 13:37:56 +0200

    rotate working

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 // Hamburger menu
-
-
 function menu(){
     const $ = document.querySelector.bind(document)
 
@@ -24,3 +22,26 @@ function menu(){
 }
 
 menu()
+
+
+// Sketchbook
+
+function Sketchbook(){
+    const $ = document.querySelector.bind(document)
+    const slider = $("#myRange");
+    const output = $("#demo");
+    output.innerHTML = slider.value;
+    slider.oninput = function() {
+    output.innerHTML = this.value;
+    $(".sketch-1").style.transform = `rotateZ(${360/100*this.value}deg)`;
+    }
+    const slider2 = $("#myRange2");
+    const output2 = $("#demo2");
+    output2.innerHTML = slider2.value;
+    slider2.oninput = function() {
+    output2.innerHTML = this.value;
+    $(".sketch-1").style.transform = `rotateX(${360/100*this.value}deg)`;
+    }
+}
+
+Sketchbook()    
