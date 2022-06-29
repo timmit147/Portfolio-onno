af2ca92f92e1a21c5b03d3aca16b695330cf3bc2
timmit147 <timmeeuwsen@hotmail.nl>
2022-06-29 19:40:10 +0200

    bug fixen

--- js/script.js
+++ js/script.js
@@ -28,28 +28,27 @@ menu()
 
 function Sketchbook(){
     const $ = document.querySelector.bind(document)
-    const slider = $("#myRange");
-    const output = $("#demo");
-    output.innerHTML = slider.value;
+    // const slider = $("#myRange");
+    // const output = $("#demo");
     let zRotate = 0;
     let xRotate = 0;
-    slider.oninput = function() {
-    output.innerHTML = this.value;
-    zRotate=this.value
-    $(".sketch").style.transform = `rotateZ(${360/100*zRotate}deg)rotateX(${360/100*xRotate}deg)`;
+    // slider.oninput = function() {
+    // zRotate=this.value
+    // $(".sketch").style.transform = `rotateZ(${zRotate}deg)rotateX(${xRotate}deg)`;
 
-    }
-    const slider2 = $("#myRange2");
-    const output2 = $("#demo2");
-    output2.innerHTML = slider2.value;
-    slider2.oninput = function() {
-    output2.innerHTML = this.value;
-    xRotate=this.value
-    $(".sketch").style.transform = `rotateZ(${360/100*zRotate}deg)rotateX(${360/100*xRotate}deg)`;
-    }
+    // }
+    // const slider2 = $("#myRange2");
+    // const output2 = $("#demo2");
+    // slider2.oninput = function() {
+    // xRotate=this.value
+    // $(".sketch").style.transform = `rotateZ(${zRotate}deg)rotateX(${xRotate}deg)`;
+    // }
 
     i = 1;
+    changePage = "true";
     $(".next").onclick = function(){
+        if(changePage == "true"){
+            changePage = "false";
         if(i < 6){
         $(`.page-${i}`).classList.add("rotate");
         setTimeout(function () {
@@ -57,58 +56,69 @@ function Sketchbook(){
         i=i+2;
        }, 0.001);
         }
+        setTimeout(function () {
+            changePage = "true";
+           }, 2000);
+    }
 
     };
     $(".previous").onclick = function(){
-            $(`.page-${i}`).classList.remove("rotate");
+        if(changePage == "true"){
+            changePage = "false";
+        i=i-2;
+        $(`.page-${i+1}`).classList.remove("rotate");
             setTimeout(function () {
-            $(`.page-${i+1}`).classList.remove("rotate");
-            i=i-2;
+                $(`.page-${i}`).classList.remove("rotate");
            }, 0.001);
+           setTimeout(function () {
+            changePage = "true";
+           }, 2000);
+    }
+
     };
 
 
-    // zoom
-    let zoom = 80;
-    let zoomTimes = 1;
-    $(".zoom-in").onclick = function(){
-        zoomTimes = zoomTimes*1.2;
-        $(".sketch").style.backgroundSize = `${zoom*zoomTimes}%`;
-    };
-    $(".zoom-out").onclick = function(){
-        zoomTimes = zoomTimes/1.2;
-        $(".sketch").style.backgroundSize = `${zoom*zoomTimes}%`;
-    };
+    // // zoom
+    // let zoom = 80;
+    // let zoomTimes = 1;
+    // $(".zoom-in").onclick = function(){
+    //     zoomTimes = zoomTimes*1.2;
+    //     $(".sketch").style.backgroundSize = `${zoom*zoomTimes}%`;
+    // };
+    // $(".zoom-out").onclick = function(){
+    //     zoomTimes = zoomTimes/1.2;
+    //     $(".sketch").style.backgroundSize = `${zoom*zoomTimes}%`;
+    // };
 
-    let isDrawing = false;
-    let x = 0;
-    let y = 0;
-    let xStart = 0;
-    let yStart = 0;
-    let xNew = 0;
-    let yNew = 0;
+    // let isDrawing = false;
+    // let x = 0;
+    // let y = 0;
+    // let xStart = 0;
+    // let yStart = 0;
+    // let xNew = 0;
+    // let yNew = 0;
 
-    $(".sketch").addEventListener('mousedown', e => {
-        xStart = e.offsetX;
-        yStart = e.offsetY;
-        isDrawing = true;
-      });
+    // $(".sketch").addEventListener('mousedown', e => {
+    //     xStart = e.offsetX;
+    //     yStart = e.offsetY;
+    //     isDrawing = true;
+    //   });
       
-      $(".sketch").addEventListener('mousemove', e => {
-        if (isDrawing === true) {
-            x = e.offsetX;
-            y = e.offsetY;
-            $(".sketch").style.backgroundPositionX = `${(x-xStart)+xNew}px`;
-            $(".sketch").style.backgroundPositionY = `${(y-yStart)+yNew}px`;
-        }
-      });
+    //   $(".sketch").addEventListener('mousemove', e => {
+    //     if (isDrawing === true) {
+    //         x = e.offsetX;
+    //         y = e.offsetY;
+    //         $(".sketch").style.backgroundPositionX = `${(x-xStart)+xNew}px`;
+    //         $(".sketch").style.backgroundPositionY = `${(y-yStart)+yNew}px`;
+    //     }
+    //   });
       
-      window.addEventListener('mouseup', e => {
-        xNew = xNew-(xStart-x);
-        yNew = yNew-(yStart-y);
-        xStart= xStart+x;
-          isDrawing = false;
-      });
+    //   window.addEventListener('mouseup', e => {
+    //     xNew = xNew-(xStart-x);
+    //     yNew = yNew-(yStart-y);
+    //     xStart= xStart+x;
+    //       isDrawing = false;
+    //   });
 
 }
 
