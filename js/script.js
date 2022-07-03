ea3079d142f73a15ccc86d0987f21692ae03a995
timmit147 <timmeeuwsen@hotmail.nl>
2022-07-03 16:46:50 +0200

    mobile and swipe

--- js/script.js
+++ js/script.js
@@ -28,21 +28,14 @@ menu()
 
 function Sketchbook(){
     const $ = document.querySelector.bind(document)
-    // const slider = $("#myRange");
-    // const output = $("#demo");
     let zRotate = 0;
     let xRotate = 0;
-    // slider.oninput = function() {
-    // zRotate=this.value
-    // $(".sketch").style.transform = `rotateZ(${zRotate}deg)rotateX(${xRotate}deg)`;
+    let isDrawing = false;
+    let x = 0;
+    let y = 0;
+    let xStart = 0;
+    let yStart = 0;
 
-    // }
-    // const slider2 = $("#myRange2");
-    // const output2 = $("#demo2");
-    // slider2.oninput = function() {
-    // xRotate=this.value
-    // $(".sketch").style.transform = `rotateZ(${zRotate}deg)rotateX(${xRotate}deg)`;
-    // }
 
     i = -1;
     $(".next").onclick = function(){
@@ -68,48 +61,47 @@ function Sketchbook(){
     }
     };
 
-
-    // // zoom
-    // let zoom = 80;
-    // let zoomTimes = 1;
-    // $(".zoom-in").onclick = function(){
-    //     zoomTimes = zoomTimes*1.2;
-    //     $(".sketch").style.backgroundSize = `${zoom*zoomTimes}%`;
-    // };
-    // $(".zoom-out").onclick = function(){
-    //     zoomTimes = zoomTimes/1.2;
-    //     $(".sketch").style.backgroundSize = `${zoom*zoomTimes}%`;
-    // };
-
-    // let isDrawing = false;
-    // let x = 0;
-    // let y = 0;
-    // let xStart = 0;
-    // let yStart = 0;
-    // let xNew = 0;
-    // let yNew = 0;
-
-    // $(".sketch").addEventListener('mousedown', e => {
-    //     xStart = e.offsetX;
-    //     yStart = e.offsetY;
-    //     isDrawing = true;
-    //   });
+    $(".sketchbook").addEventListener('mousedown', e => {
+        xStart = e.offsetX;
+        yStart = e.offsetY;
+        isDrawing = true;
+      });
       
-    //   $(".sketch").addEventListener('mousemove', e => {
-    //     if (isDrawing === true) {
-    //         x = e.offsetX;
-    //         y = e.offsetY;
-    //         $(".sketch").style.backgroundPositionX = `${(x-xStart)+xNew}px`;
-    //         $(".sketch").style.backgroundPositionY = `${(y-yStart)+yNew}px`;
-    //     }
-    //   });
+      $(".sketchbook").addEventListener('mousemove', e => {
+        if(isDrawing == true){
+            x = e.offsetX;
+            y = e.offsetY;
+            
+            console.log(xRotate);
+            xRotate =  ((yStart - y)/5) + xRotate;
+
+            if(xRotate < 0 ){
+                xRotate = 0;
+            }
+            if(xRotate > 90){
+                xRotate = 90;
+            }
+
+
+            zRotate = ((xStart - x)/5) + zRotate;
+            
+            if(zRotate < 0){
+                zRotate = 0;
+            }
+            if(zRotate > 90){
+                zRotate = 90;
+            }
+
+
+
+            $(".sketch").style.transform = `rotateZ(${zRotate}deg)rotateX(${xRotate}deg)`;
+        }
+        
+      });
       
-    //   window.addEventListener('mouseup', e => {
-    //     xNew = xNew-(xStart-x);
-    //     yNew = yNew-(yStart-y);
-    //     xStart= xStart+x;
-    //       isDrawing = false;
-    //   });
+      window.addEventListener('mouseup', e => {
+        isDrawing = false;
+      });
 
 }
 
@@ -127,7 +119,7 @@ function MouseCircle(){
         circle.style.top = e.pageY + 'px';
     });
     window.addEventListener('scroll', function(e) {
-        if(window.scrollY > 0){
+        if(window.scrollY > 200){
             circle.classList.add("hide");
         }
         else{
